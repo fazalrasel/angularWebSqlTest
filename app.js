af6277b2ca3b55588ecd0b28ed1dc0ab4124c155
FazalRasel <rasel20062007@gmail.com>
2015-09-23 22:31:17 +0600

    Rearrage index.html

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
 (function () {
     "use strict";
-    //document.addEventListener()
 
     angular.module('websqlTest', ["common", "modelTest"])
-
-
-    document.addEventListener('deviceready', onDeviceReady, false);
-
-    function onDeviceReady() {
-        console.log('device ready');
-        angular.module('websqlTest')
-            .run(function (appStartService) {
-                appStartService.appStart();
-            });
-    }
-
-
+        .run(function (appStartService) {
+            appStartService.appStart();
+        });
 }());
